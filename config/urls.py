d6f2628703a517807e67b390ab3578d459fe6190
fracisconeto <vargasfrancisco227@gmail.com>
2025-02-19 11:07:04 -0300

    Introdução API REST

--- config/urls.py
+++ config/urls.py
@@ -2,10 +2,13 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from alunos.views import EstadoViewSet
+from alunos.views import EstadoViewSet, CidadeViewSet, AlunoViewSet
+
 
 router = DefaultRouter()
-router.register(r'estados', EstadoViewSet)
+router.register(r'estados', EstadoViewSet,)
+router.register(r'cidade', CidadeViewSet,)
+router.register(r'aluno', AlunoViewSet,)
 
 urlpatterns = [
     path('', include(router.urls)),
